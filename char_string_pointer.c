83e0c84e37638a2f3ac805c0bbf02152a2afd36d
VectorLu <vectorpinglu@outlook.com>
2016-04-18 10:26:00 +0800

    Create char_string_pointer.c
    用指向字符串的指针数组来存储字符串列表

---
+++ char_string_pointer.c
@@ -0,0 +1,19 @@
+//用指向字符串的指针数组来存储字符串列表
+
+#include <stdio.h>
+
+const int MAX = 4;
+
+int main() {
+    int *char_string[4] = {
+        "string_1",
+        "string_2",
+        "string_3",
+        "string_4"
+    };
+    for(int i=0; i < 4; i++){
+        printf("%s\n", char_string[i]);
+    }
+    
+    return 0;
+}
