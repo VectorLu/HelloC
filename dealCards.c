53f8ffa6814fca84cc9f8f9f1952118d197fbdbb
VectorLu <vectorpinglu@outlook.com>
2016-04-19 19:59:11 +0800

    Update dealCards.c

--- dealCards.c
+++ dealCards.c
@@ -2,31 +2,31 @@
 #include <time.h>
 
 void shuffle(int wDeck[4][13]);
-void deal(int wDeck[4][13], char wSuit[4], char *wFace);
+void deal(int wDeck[4][13], char *wSuit[4], char *wFace[13]);
 
-int main(int argc, char const *argv[])
+int main()
 {
-    char wSuit[4] = {'\x3', '\x4', '\x5', 'x6'};
-    char wFace[13] = {
-        "Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
-        "Jake", "Queen", "King"
-    };
-    
-    int wDeck [4][13] = {0};
-    shuffle(wDeck);
-    deal(wDeck, wSuit, wFace);
-    
-    return 0;
+	char *wSuit[4] = { "\x3", "\x4", "x5", "\x6"};
+	char *wFace[13] = {
+		"Ace", "Deuce", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+		"Jake", "Queen", "King"};
+	srand((unsigned int )time(NULL));
+
+	int wDeck[4][13] = { 0 };
+	shuffle(wDeck);
+	deal(wDeck, wSuit, *wFace);
+
+	return 0;
 }
 
-void shuffle(int wDeck[4][13]){
-    int random;
-    int card, row, column;
-    
-    for(card=1; card<=52; card++){
-        random = rand();
-        row = random % 4;
-        printf("%d ", row);
-        
-    }
+void shuffle(int wDeck[4][13]) {
+	int random;
+	int card, row, column;
+
+	for (card = 1; card <= 52; card++) {
+		random = rand();
+		row = random % 4;
+		printf("%d ", row);
+
+	}
 }
