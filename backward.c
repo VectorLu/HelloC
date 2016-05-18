bef887cb69fcc14131a77f4a03dc4889b538b2d1
VectorLu <vectorpinglu@outlook.com>
2016-05-18 18:32:49 +0800

    correct
    correct the name of variables

--- backward.c
+++ backward.c
@@ -1,18 +1,18 @@
 //将输入的整数逆序输出
-//
+//test git 
+//如果在VS里运行，scanf要换为scanf_s
 #include <stdio.h>
-
 int main(){
-	int num;
-	int ret;
-	int digit;
-	//如果在VS里运行，scanf要换为scanf_s
-	scanf("%d", &num);
-	while(num > 0){
-		digit = num%10;
-		ret = ret*10 + digit;
-		num /=10;
-	}
-	printf("%d", ret);
-	return 0;
-}	
+    int digit = 0;
+    int remainder = 0;
+    int back = 0;
+    scanf("%d",&digit);
+    
+    while(digit > 0){
+        remainder = digit % 10;
+        back = 10*back + remainder;
+        digit /= 10;
+    }
+    printf("%d\n", back);
+    return 0;
+}
