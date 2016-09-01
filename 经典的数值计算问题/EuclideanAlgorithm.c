eb4c5f640fea108b22c03346fabc3bcbe0dfbf46
VectorLu <vectorpinglu@outlook.com>
2016-09-01 21:44:22 +0800

    Update EuclideanAlgorithm.c

--- 经典的数值计算问题/EuclideanAlgorithm.c
+++ 经典的数值计算问题/EuclideanAlgorithm.c
@@ -7,7 +7,7 @@ int main(){
     int temp_m, temp_n;
     int remainder = 0;
     int greatest_common_divisor = 1;
-    int largest_commo_multiple = 1;
+    int lowest_common_multiple = 1;
     
     scanf("%d%d", &m, &n);
     temp_m = m;
@@ -21,7 +21,7 @@ int main(){
     printf("The greatest common divisor of %5d and %5d"
             " is %5d.\n", m, n, greatest_common_divisor);
     
-    largest_commo_multiple = m * n /greatest_common_divisor;
+    lowest_common_multiple = m * n /greatest_common_divisor;
     printf("The largest common multiple of %5d and %5d"
            " is %5d.\n", m, n, largest_commo_multiple);
     
